9d2d934e387c5460c93149a3aac25bf894bdae94
Arun7Raju <aruna@goschedule.io>
2023-10-26 20:30:02 +0530

    changes the auth bug fix
    Signed-off-by: Arun7Raju <aruna@goschedule.io>

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next)=>{
         const isCustomAuth = token.length < 500;
         let decodedData;
         if(token && isCustomAuth){
-            decodedData = jwt.verify(token, 'secret');
+            decodedData = jwt.verify(token, process.env.SECRET);
             req.userId = decodedData?.id;
             req.email = decodedData?.email;
         }else{
